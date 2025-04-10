5c314710543e4be118050b982b7cd90979aed562
cvargas306 <cavbcr@gmail.com>
2025-04-09 23:30:27 -0600

    Gitignore modified

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongodb = require('./data/database');
 const app = express();
 
@@ -6,6 +7,7 @@ const port = process.env.PORT || 3000;
 
 
 app.use('/', require('./routes'));
+app.use(bodyParser.json());
 
 
 mongodb.initDb((err) => {
