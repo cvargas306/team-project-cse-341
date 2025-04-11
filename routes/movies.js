b6f0c7f3dd06aae45b661191ec2ad37e051c862f
cvargas306 <cavbcr@gmail.com>
2025-04-11 15:51:21 -0600

    routes/movies.js modified

--- routes/movies.js
+++ routes/movies.js
@@ -3,16 +3,16 @@ const router = express.Router();
 
 const moviesController = require('../controllers/movies');
 const validation = require('../middleware/validate');
-const { isAuthenticated } = require("../middleware/authenticate");
+//const { isAuthenticated } = require("../middleware/authenticate");
 
 router.get('/', moviesController.getAll);
 
 router.get('/:id', moviesController.getSingle);
 
-router.post('/', isAuthenticated, validation.saveMovie, moviesController.createMovie);
+router.post('/', validation.saveMovie, moviesController.createMovie);
 
-router.put('/:id', isAuthenticated, validation.saveMovie, moviesController.updateMovie);
+router.put('/:id', validation.saveMovie, moviesController.updateMovie);
 
-router.delete('/:id', isAuthenticated, moviesController.deleteMovie);
+router.delete('/:id', moviesController.deleteMovie);
 
 module.exports = router;
