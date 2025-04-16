0d74b51ad82a3bedefa43a754d411e6c162759b2
cvargas306 <cavbcr@gmail.com>
2025-04-15 20:25:46 -0600

    JWT code added

--- routes/movies.js
+++ routes/movies.js
@@ -3,16 +3,22 @@ const router = express.Router();
 
 const moviesController = require('../controllers/movies');
 const validation = require('../middleware/validate');
-const { isAuthenticated } = require("../middleware/authenticate");
+//const { isAuthenticated } = require("../middleware/authenticate");
+const authenticateJWT = require('../middleware/authenticateJWT');
 
 router.get('/', moviesController.getAll);
 
 router.get('/:id', moviesController.getSingle);
 
-router.post('/', isAuthenticated, validation.saveMovie, moviesController.createMovie);
+// router.post('/', isAuthenticated, validation.saveMovie, moviesController.createMovie);
 
-router.put('/:id', isAuthenticated, validation.saveMovie, moviesController.updateMovie);
+// router.put('/:id', isAuthenticated, validation.saveMovie, moviesController.updateMovie);
 
-router.delete('/:id', isAuthenticated, moviesController.deleteMovie);
+// router.delete('/:id', isAuthenticated, moviesController.deleteMovie);
+
+// Protected routes
+router.post('/', authenticateJWT, validation.saveMovie, moviesController.createMovie);
+router.put('/:id', authenticateJWT, validation.saveMovie, moviesController.updateMovie);
+router.delete('/:id', authenticateJWT, moviesController.deleteMovie);
 
 module.exports = router;
